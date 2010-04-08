e0355d86b67a25a170d6eedb6e5c45b4fd0f1883
Paweł Obrok <pawel.obrok@gmail.com>
2010-04-08 14:18:28 +0200

    Ticket #5 Dodalem test do logowania niepoprawnym haslem

--- frontend/spec/requests/login_spec.rb
+++ frontend/spec/requests/login_spec.rb
@@ -20,4 +20,15 @@ describe "Not logged in user" do
   it "cannot access the main controller" do
     response_status.should == 401
   end
+
+  it "should see the login form" do
+    response.should include "Email"   
+  end
+
+  it "should have to provide correct data to login" do
+    @user = create_user
+    fill_in "Email", :with => @user.email
+    click_button "Log In"
+    response_status.should == 401
+  end
 end
