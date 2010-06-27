5b43caf21888b6dd7c607d7ce456e2405f8fb02c
Marcin Nowak <mn@buntu.(none)>
2010-06-27 13:23:50 +0200

    test do widoku tokenow

--- frontend/spec/requests/tokens_spec.rb
+++ frontend/spec/requests/tokens_spec.rb
@@ -15,4 +15,20 @@ describe Tokens do
     response.should include("Tokeny dla ankiety")
     response.should include(poll.name)
   end
+
+  it "should show correct amount of valid tokens after tokens generation" do
+    poll = create_poll(:user => @user)
+    visit '/polls' / poll.id / 'edit'
+    click_link 'Generuj tokeny'
+    fill_in "Ilość tokenów", :with => 20
+    valid_until = "2100-01-01"
+    fill_in "Data ważności", :with => valid_until
+    click_button 'Generuj tokeny'
+
+    response.should include(valid_until)
+    poll.tokens.size.should be_equal 20
+    poll.tokens.each do |token|
+      response.should include(token.value)
+    end
+  end
 end
