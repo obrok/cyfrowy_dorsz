62f9136496a0a398002eb1d14bfea38e3018b672
obrok <pawel.obrok@gmail.com>
2010-07-04 21:04:06 +0200

    Poprawka testów

---
+++ frontend/spec/destruction_test_helper.rb
@@ -0,0 +1,22 @@
+class User < Sequel::Model
+  def before_destroy
+    super
+    polls.each{|x| x.destroy}
+  end
+end
+
+class Poll < Sequel::Model
+  def before_destroy
+    super
+    questions.each{|x| x.destroy}
+    answers.each{|x| x.destroy}
+    tokens.each{|x| x.destroy}
+  end
+end
+
+class Question < Sequel::Model
+  def before_destroy
+    super
+    question_answers.each{|x| x.destroy}
+  end
+end
