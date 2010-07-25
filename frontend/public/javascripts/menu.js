c2fd4d544b9fa0fcaa259860ee669a50426b1535
Ania Leśniak <lesniakania@gmail.com>
2010-07-25 13:40:54 +0200

    Drobne zmiany w menu/poprawki wygladu w calej aplikacji

--- frontend/public/javascripts/menu.js
+++ frontend/public/javascripts/menu.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
+  $('ul#menu > li').each(function() {
+    $(this).removeClass('active');
+    var activeUrl = window.location.pathname.match(/\w+/).toString();
+    var url = $(this).find('a').first().attr('href').match(/\w+/).toString();
+    if(url == activeUrl) {
+      $(this).addClass('active');
 
-  $('ul#menu > li').mouseover(function() {
-    $('ul#menu > li > ul').addClass('hidden');
-    $(this).children().each(function() {
-      $(this).removeClass('hidden');
-    });
+      $('ul#menu > li > ul').addClass('hidden');
+      $(this).children().each(function() {
+        $(this).removeClass('hidden');
+      });
+    }
   });
-
-  $('ul#menu > li').mouseout(function() {
-    $('ul#menu > li > ul').addClass('hidden');
-    $('ul#menu > li.active > ul').removeClass('hidden');
-  });
-
 });
