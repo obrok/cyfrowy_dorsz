35405516755e65176b34a32b7b2bd585d1baae15
Ania Leśniak <lesniakania@gmail.com>
2010-09-28 00:06:37 +0200

    Poprawione podswietlanie

--- frontend/public/javascripts/menu.js
+++ frontend/public/javascripts/menu.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   $('ul#menu > li').each(function() {
     $(this).removeClass('active');
-    var activeUrl = window.location.pathname.match(/\w+/).toString();
-    var url = $(this).find('a').first().attr('href').match(/\w+/).toString();
+    var activeUrl = window.location.pathname.match(/\w+/);
+    if (activeUrl) {
+      activeUrl = activeUrl.toString();
+    } else {
+      activeUrl = '/';
+    }
+
+    var url = $(this).find('a').first().attr('href').match(/\w+/);
+    if (url) {
+      url = url.toString();
+    } else {
+      url = '/';
+    }
+
     if(url == activeUrl) {
       $(this).addClass('active');
 
