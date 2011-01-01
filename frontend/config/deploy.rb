8d99ddef769bb4a21c9cb572adea272db7c627ee
Ania <lesniakania@gmail.com>
2011-01-01 19:25:35 +0100

    Configured capistrano

--- frontend/config/deploy.rb
+++ frontend/config/deploy.rb
@@ -1,20 +1,33 @@
+require 'bundler/capistrano'
+
 set :application, "cyfrowy_dorsz"
 set :repository,  "git@codegarden.icsadl.agh.edu.pl:cyfrowy_dorsz"
-
 set :scm, :git
+set :deploy_to, "/home/ankieter/#{application}/frontend/"
 
 role :web, "codegarden.icsadl.agh.edu.pl:4000"
 role :app, "codegarden.icsadl.agh.edu.pl:4000"
 role :db,  "codegarden.icsadl.agh.edu.pl:4000", :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
+
+  desc "Link in the production extras and Migrate the Database"
+  task :after_update_code do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/merb.yml #{release_path}/config/merb.yml"
+    run "ln -nfs #{shared_path}/log #{release_path}/log"
+    deploy.migrate
+  end
+
+  desc "Restart server"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Migrate database"
+  deploy.task :migrate do
+    run "cd #{release_path}; rake sequel:db:migrate MERB_ENV=production"
   end
 end
