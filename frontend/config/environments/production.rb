5c7837466c8c0cbbc3f8f270173c5cec4dda6610
obrok <pawel.obrok@gmail.com>
2011-01-22 21:31:10 +0100

    Dodane hostname do environments/production

--- frontend/config/environments/production.rb
+++ frontend/config/environments/production.rb
@@ -5,6 +5,7 @@ Merb::Config.use { |c|
   c[:log_level] = :error
   
   c[:log_file]  = Merb.root / "log" / "production.log"
+  c[:hostname] = "s2.rootnode.net"
   # or redirect logger using IO handle
   # c[:log_stream] = STDOUT
 }
