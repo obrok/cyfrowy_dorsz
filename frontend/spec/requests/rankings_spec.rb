a22975f22c3cee2e25de4d59ad14300f0bd6f5cd
obrok <pawel.obrok@gmail.com>
2011-02-26 19:50:50 +0100

    Removed emails from ranking page

--- frontend/spec/requests/rankings_spec.rb
+++ frontend/spec/requests/rankings_spec.rb
@@ -1,24 +1,31 @@
+# -*- coding: utf-8 -*-
 require 'spec/spec_helper'
 
 describe Rankings do
   before(:each) do
-    @user = create_user
+    @user = create_user(:name => "Jan", :surname => "Kowalski")
     poll = create_poll(:user => @user)
     question = create_question(:poll => poll, :question_type => Question::TYPES[:closed])
     create_question_answer(:question => question, :value => 1)
     create_question_answer(:question => question, :value => 4)
 
-    @user2 = create_user
+    @user2 = create_user(:name => "Zbigniew", :surname => "Buła")
     poll2 = create_poll(:user => @user2)
     question2 = create_question(:poll => poll2, :question_type => Question::TYPES[:closed])
     create_question_answer(:question => question2, :value => 5)
     create_question_answer(:question => question2, :value => 4)
   end
 
-  it "should contain user's e-mail" do
+  it "should contain user's name and surname" do
+    visit '/rankings'
+    response.should include(@user.name)
+    response.should include(@user.surname)
+  end
+
+  it "should not contain user's e-mail" do
     visit '/rankings'
 
-    response.should include(@user.email)
+    response.should_not include(@user.email)
   end
 
   it "should contain user's ranking" do
@@ -30,6 +37,6 @@ describe Rankings do
   it "should display users in correct order" do
     visit '/rankings'
 
-    response.index(@user2.email).should < response.index(@user.email)
+    response.index(@user2.name).should < response.index(@user.name)
   end
 end
