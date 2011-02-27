a6c7e59dd79d9d7d1d77414f9845de7fcbd37ce7
Pawel Pierzchala <pawelpierzchala@gmail.com>
2011-02-27 22:39:21 +0100

    refactoring modelu

--- frontend/app/models/question.rb
+++ frontend/app/models/question.rb
@@ -67,10 +67,11 @@ class Question < Sequel::Model
   def validate
     super
     validates_unique [:poll_id, :text], :message => "Pytanie o takiej nazwie już istnieje"
+    validates_unique([:question_type], :message => "Istnieje juz pytanie o prowadzacego") { |ds| ds.filter(:question_type => TYPES[:teacher]).filter(:poll_id => poll_id) }
     validates_presence :text, :message => "treść pytania jest wymagana"
     validates_presence :poll
+    validates_includes TYPES.values, :question_type, :message => "niepoprawny typ pytania"
     errors[:possible_answer] << "Wymagana przynajmniej jedna mozliwa odpowiedz" if choice? && (possible_answers.blank? || possible_answers.empty?)
-    errors[:question_type] << "niepoprawny typ pytania" unless TYPES.values.include?(question_type)
   end
 
   def open?
